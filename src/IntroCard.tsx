712665dcb62752fc28fb1b93d3e28de18a58432a
Anand Patel <anandkumarpatel@gmail.com>
2024-04-10 11:16:17 -0700

    update questions

--- src/IntroCard.tsx
+++ src/IntroCard.tsx
@@ -3,10 +3,11 @@ import { FunctionComponent } from 'react'
 const IntoCard: FunctionComponent = () => {
   return (
     <div className='title-card'>
-      <h1>You want the shoes?</h1>
+      <h1>Team Neha: You want the shoes?</h1>
       <p className='quiz-info'>
-        You have 1 hour to discover the code and find the key! Let's see if Akhil's Family & Friends have what it takes! Type the answers to the questions in the boxes. If you type
-        the answer correctly, the box will turn green. Answers can be entered on any phone and in any order. The number on the right tells you how long the word is. Good luck!
+        ou have 1 hour to discover the code and find the key! Let's see if Neha's Family & Friends have what it takes to answer questions about Akhil's side and figure out the
+        code! Type the answers to the questions in the boxes. If you type the answer correctly, the box will turn green. Answers can be entered on any phone and in any order. The
+        number on the right tells you how long the word is. Good luck!
       </p>
     </div>
   )
