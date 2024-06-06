0d96311c97408d57d0683b88788921677f140090
Anand Patel <anandkumarpatel@gmail.com>
2024-06-06 12:59:00 -0700

    update win condition

--- src/FinalCard.tsx
+++ src/FinalCard.tsx
@@ -3,8 +3,8 @@ import { FunctionComponent } from 'react'
 const FinalCard: FunctionComponent = () => {
   return (
     <div className='title-card'>
-      <h1>Now Open The Lock!</h1>
-      <p className='quiz-info'>The lock code starts with 0 and the other 2 digits have something to do with CaPiTalS.</p>
+      <h1>Great Job!</h1>
+      <p className='quiz-info'>You are OwlMazing!!</p>
     </div>
   )
 }
