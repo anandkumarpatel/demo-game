4cd710ead4e67b16aecf01c5f638826e6c190269
Anand Patel <anandkumarpatel@gmail.com>
2024-06-05 17:00:02 -0700

    glassify

--- src/IntroCard.tsx
+++ src/IntroCard.tsx
@@ -5,7 +5,7 @@ const IntoCard: FunctionComponent = () => {
     <div className='title-card'>
       <h1>Its Game Time!</h1>
       <p className='quiz-info'>
-        Type the answers to the questions in the boxes. If you (or anyone else) type the answer correctly, the box will turn change. Answers can be entered on any phone and in any
+        Type the answers to the questions in the boxes. If you (or anyone else) type the answer correctly, the box will change. Answers can be entered on any phone and in any
         order. The number on the right tells you how long the word is. Good luck!
       </p>
     </div>
