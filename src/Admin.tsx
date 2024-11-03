58fa33deb71e60e55771a60afac2fdf631ad8cdd
Anand Patel <anandkumarpatel@gmail.com>
2024-11-03 12:50:20 -0800

    update click count

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Box } from '@mui/material'
 
 const path = window.location.pathname
-console.log('path', path)
+
 const Admin: React.FC = () => {
   return (
     <Box sx={{ width: '100vw', height: '100vh', overflow: 'scroll', gap: '2rem', display: 'flex', flexDirection: 'column', padding: '2rem' }}>
--- src/ClickCard.tsx
+++ src/ClickCard.tsx
@@ -6,8 +6,7 @@ type Props = {
   onClick: () => void
 }
 
-// const COUNT = 350 * 10 // 350 is how many I click per minute. mult this by people we expect to play
-const COUNT = 2 // 350 is how many I click per minute. mult this by people we expect to play
+const COUNT = 351 * 10 // 350 is how many I click per minute. mult this by people we expect to play
 const ClickCard: React.FC<Props> = ({ otherCount, onFinish, onClick }) => {
   const [count, setCount] = React.useState(0)
   const c = Math.max(count, otherCount)
