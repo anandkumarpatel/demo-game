1e84d142a594b5866869e2185ec0caa09f7487e7
Anand Patel <anandkumarpatel@gmail.com>
2024-11-03 09:23:14 -0800

    update text

--- src/IntroCard.tsx
+++ src/IntroCard.tsx
@@ -5,8 +5,8 @@ const IntoCard: FunctionComponent = () => {
     <div className='title-card'>
       <h1>You Want The Shoes?!</h1>
       <p className='quiz-info'>
-        Time to see if you know JD well enough to get deserve his shoes. Type the answers to the questions in the boxes. If you (or anyone else) type the answer correctly, the box
-        will change. Answers can be entered on any phone and in any order. The number on the right tells you how long the word is. Good luck!
+        Time to see if you know JD well enough to deserve his shoes. Type the answers to the questions in the boxes. If you (or anyone else) type the answer correctly, the box will
+        change. Answers can be entered on any phone and in any order. The number on the right tells you how long the word is. Good luck!
       </p>
     </div>
   )
